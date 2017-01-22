5d48a1a65b4c6140ed903a21b10dc1871c4d6494
tonychen <119185964@qq.com>
2017-01-22 15:20:08 +0800

    初始化项目

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/k8sManager/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/k8sManager/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
