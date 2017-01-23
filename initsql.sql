98ec409ea8b65a319f3788fcebba949edc85d6d4
tonychen <119185964@qq.com>
2017-01-23 16:53:23 +0800

    登陆验证

---
+++ initsql.sql
@@ -0,0 +1,20 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `login_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `enable` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES ('2', '2017-01-23 15:09:06', '2017-01-23 15:09:06', '系统管理员', 'admin', '96e79218965eb72c92a549dd5a330112', '1');
